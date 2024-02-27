9009d91b481555745981afef9704c97d93a13783
Nasratullah Nasrat <60459439+Nasratullahn@users.noreply.github.com>
2024-02-27 11:51:42 +0000

    added

--- tests/test_calculator.rb
+++ tests/test_calculator.rb
@@ -1,3 +1,5 @@
+# tests/test_calculator.rb
+
 require 'minitest/autorun'
 require './calculator'
 
@@ -6,4 +8,9 @@ class TestCalculator < Minitest::Test
     calculator = Calculator.new
     assert_equal 4, calculator.add(2, 2), "Addition method failed"
   end
+
+def test_subtraction
+  calculator = Calculator.new
+  assert_equal 0, calculator.subtract(2, 2), "Subtraction method failed"
+end
 end
